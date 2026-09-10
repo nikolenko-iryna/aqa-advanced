5065d03949e6386d1ed0ca6b95e1d548e37d3730
Iryna Nikolenko <irinanikolenko72@gmail.com>
2026-09-10 09:43:32 +0300

    Додано завдання з використання цикл for і while

---
+++ loopsBasics/task3.js
@@ -0,0 +1,12 @@
+let num = 8;
+for (let i= 1; i <= 10; i++) {
+    let result = num * i;
+    console.log(`${num} * ${i} = ${result}`);
+}
+
+let i = 1;
+while (i <= 10) {
+let result = num * i;
+    console.log(`${num} * ${i} = ${result}`);
+    i++;
+}
